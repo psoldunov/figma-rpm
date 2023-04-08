bb0894e4e4db7c792976343e1a95a2ee54933978
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2023-04-08 16:32:25 +0300

    upload files

---
+++ main.js
@@ -0,0 +1,30 @@
+const { app, BrowserWindow, shell } = require('electron');
+
+app.on('ready', () => {
+	const win = new BrowserWindow({
+		autoHideMenuBar: true,
+		width: 1920,
+		height: 1080,
+		minWidth: 800,
+		minHeight: 600,
+	});
+
+	// Handle all navigation events
+	win.webContents.on('did-navigate', (event, url) => {
+		if (url === 'https://www.figma.com/') {
+			win.loadURL('https://www.figma.com/login');
+		}
+	});
+
+	// Load the login page
+	win.loadURL('https://www.figma.com/login');
+
+	// Handle external links
+	win.webContents.setWindowOpenHandler(({ url }) => {
+		if (!url.startsWith('https://www.figma.com/')) {
+			shell.openExternal(url);
+			return { action: 'deny' };
+		}
+		return { action: 'allow' };
+	});
+});
