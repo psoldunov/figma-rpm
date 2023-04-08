06ba3b4286957739d7a29dd037bf6d4b0726c3c3
Philipp Soldunov <69530789+psoldunov@users.noreply.github.com>
2023-04-08 16:44:38 +0300

    add electron-store support for previous state

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 const { app, BrowserWindow, shell } = require('electron');
+const Store = require('electron-store');
+
+// Create a new instance of the electron-store module
+const store = new Store();
 
 app.on('ready', () => {
+	let mainWindowBounds = store.get('windowBounds');
+	if (!mainWindowBounds) {
+		mainWindowBounds = {
+			x: 0,
+			y: 0,
+			width: 1920,
+			height: 1080,
+		};
+	}
+
 	const win = new BrowserWindow({
 		autoHideMenuBar: true,
-		width: 1920,
-		height: 1080,
+		...mainWindowBounds,
 		minWidth: 800,
 		minHeight: 600,
 	});
+
+	// Load the login page
+	win.loadURL('https://www.figma.com/files');
 
 	// Handle all navigation events
 	win.webContents.on('did-navigate', (event, url) => {
@@ -16,9 +32,6 @@ app.on('ready', () => {
 		}
 	});
 
-	// Load the login page
-	win.loadURL('https://www.figma.com/files');
-
 	// Handle external links
 	win.webContents.setWindowOpenHandler(({ url }) => {
 		if (!url.startsWith('https://www.figma.com/')) {
@@ -26,5 +39,16 @@ app.on('ready', () => {
 			return { action: 'deny' };
 		}
 		return { action: 'allow' };
+	});
+
+	// Save the window position and size when the window is moved or resized
+	win.on('resize', () => {
+		const bounds = win.getBounds();
+		store.set('windowBounds', bounds);
+	});
+
+	win.on('move', () => {
+		const bounds = win.getBounds();
+		store.set('windowBounds', bounds);
 	});
 });
